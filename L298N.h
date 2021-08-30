25848614b410ee756659d26e9017e83359dd8269
Divyank Shah <36285610+shahdivyank@users.noreply.github.com>
2021-08-29 21:37:51 -0700

    recreate functions

--- L298N.h
+++ L298N.h
@@ -1,31 +1,46 @@
- #include "Arduino.h"
- #ifndef L298N_h
- #define L298N_h
+#ifndef L298N_h
+#define L298N_h
 
- class L298N {
-    private:
-      byte enablePin;
-      byte forwardPin;
-      byte backwardPin;
-      byte motorSpeed;
-      bool motorDirection;
-      
-    public:
-      L298N(byte enable, byte forward, byte backward);
-      void attachMotor();
-      void stopMotor();
-      void defineSpeed(byte velocity);
-      byte getSpeed();
-      bool getDirection();
-      void setVelocity(String directionType, byte velocity, String velocityUnits);
-      void spin();
-      void spin(String directionType, byte velocity, String velocityUnits);
-      void rotateFor(unsigned int delayTime, String directionType, byte velocity, String velocityUnits);
-      void spinForward();
-      void spinForward(byte velocity, String velocityUnits);
-      void spinBackward();
-      void spinBackward(byte velocity, String velocityUnits);
-      
- };
+#include "Arduino.h"
 
- #endif
+class L298N {
+   private:
+     byte _enable;
+     byte _forward;
+     byte _backward;
+     byte _speed;
+     bool _direction;
+     bool _isMoving;
+
+   public:
+     L298N();
+     L298N(byte enable, byte forward, byte backward);
+
+     void setEnable(byte enable);
+     byte getEnable();
+
+     void setForward(byte forward);
+     byte getForward();
+
+     void setBackward(byte backward);
+     byte getBackward();
+
+     void setSpeed(byte speed, String units);
+     byte getSpeed();
+
+     void setDirection(String direction);
+     string getDirection();
+
+     void stop();
+
+     void spin();
+     void spin(String direction);
+     void spin(String direction, byte speed, String units);
+
+     void rotateFor(unsigned int time);
+     void rotateFor(unsigned int time, String direction);
+     void rotateFor(unsigned int time, String direction, byte speed, String units);
+
+};
+
+#endif
